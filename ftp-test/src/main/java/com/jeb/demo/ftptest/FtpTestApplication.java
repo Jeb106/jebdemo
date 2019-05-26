ab8e2e5cfcbfcb6424598b47a6ab711ef51140b3
jeb <jinbiao106@sina.com>
2019-05-26 11:52:03 +0800

    ftp测试

---
+++ ftp-test/src/main/java/com/jeb/demo/ftptest/FtpTestApplication.java
@@ -0,0 +1,13 @@
+package com.jeb.demo.ftptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpTestApplication.class, args);
+	}
+
+}
