08818f41bd7d4c7fe2162ce1ba8fee9370e7dda6
jeb <jinbiao106@sina.com>
2019-12-07 16:38:19 +0800

    开启https

--- demohttps/src/main/java/com/jeb/https/demo/demo/DemohttpsApplication.java
+++ demohttps/src/main/java/com/jeb/https/demo/demo/DemohttpsApplication.java
@@ -2,15 +2,35 @@ package com.jeb.https.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemohttpsApplication {
+public class DemohttpsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemohttpsApplication.class, args);
 	}
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 
 
 }
+
+//
+//public class DemohttpsApplication extends SpringBootServletInitializer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(DemohttpsApplication.class, args);
+//	}
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(this.getClass());
+//	}
+//
+//
+//}
